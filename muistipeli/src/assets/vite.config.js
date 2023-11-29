1d273d11d587948e355985fe3a63f5b62b85d23a
Pauli Selenius <99865089+Mera212@users.noreply.github.com>
2023-11-29 10:26:11 +0200

    gh-pages2

--- muistipeli/src/assets/vite.config.js
+++ muistipeli/src/assets/vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./muistipeli",
+  base: "/Ohjelmointikehitys1.5/muistipeli",
   plugins: [react()],
-})
+});
